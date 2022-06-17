12c0b15d3e9e17cd6c4e77f9fb3fef5c20c7fd29
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-06-17 23:53:32 +1000

    JobDistanceRate

--- src/components/JobDistanceRate/JobDistanceRate.js
+++ src/components/JobDistanceRate/JobDistanceRate.js
@@ -3,7 +3,17 @@ import './JobDistanceRate.css'
 
 function JobDistanceRate() {
   return (
-    <div>JobDistanceRate</div>
+    <div className="job-distance-rate-container">
+        <div className="distance-container">
+        <p className="job-distance-rate-header">Distance</p>
+        <p className="job-distance-rate-data">5.6 miles</p>
+
+        </div>
+        <div className="rate-container">
+        <p className="job-distance-rate-header">Hourly Rate</p>
+        <p className="job-distance-rate-data"><span className="super">$</span>13.50</p>
+        </div>
+    </div>
   )
 }
 
