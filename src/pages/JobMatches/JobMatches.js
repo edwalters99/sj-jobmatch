92c15f26739fd61171486296cab4ecfc9cf30970
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-06-18 20:14:52 +1000

    getProfile API call, spinner when loading, error rendering

--- src/pages/JobMatches/JobMatches.js
+++ src/pages/JobMatches/JobMatches.js
@@ -1,16 +1,72 @@
-import React from 'react';
-import './JobMatches.css'
-import Header from '../../components/Header/Header';
-import JobCard from '../../components/JobCard/JobCard';
+import React, { useState, useEffect } from "react";
+import "./JobMatches.css";
+import Header from "../../components/Header/Header";
+import JobCard from "../../components/JobCard/JobCard";
+import ClipLoader from "react-spinners/ClipLoader";
+import { AiOutlineReload } from "react-icons/ai";
+const axios = require("axios");
 
+const SERVER_URL = "https://test.swipejobs.com/api";
+
+const WORKER_ID = "7f90df6e-b832-44e2-b624-3143d428001f";
+// for test purposes. When operational this would be passed in as a prop.
 
 function JobMatches() {
+  const [loading, setLoading] = useState(true);
+  const [profileData, setProfileData] = useState({});
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getProfile(WORKER_ID);
+  }, []);
+
+  async function getProfile(workerID) {
+    try {
+      const response = await axios.get(
+        SERVER_URL + "/worker/" + workerID + "/profile"
+      );
+      console.log("response ", response);
+      setLoading(false);
+      setProfileData(response.data);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  }
+
+  if (loading) {
+    return (
+      <div className="loading-spinner">
+        <ClipLoader />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="error-body">
+        <div className="error-container">
+          <p className="message">Sorry! Something has gone wrong: </p>
+          <p className="error">{error}</p>
+          <div
+            className="reload-button"
+            onClick={() => {
+              window.location.reload(false);
+            }}
+          >
+            <AiOutlineReload size={40} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
       <JobCard />
     </div>
-  )
+  );
 }
 
-export default JobMatches
+export default JobMatches;
