ea0478f8fb68ebe99dcbc09550dbe765d0aa121b
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-06-18 13:18:40 +1000

    JobRequirements

--- src/components/JobRequirements/JobRequirements.js
+++ src/components/JobRequirements/JobRequirements.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {GoTools} from 'react-icons/go'
 import './JobRequirements.css'
 
 function JobRequirements() {
   return (
-    <div>JobRequirements</div>
+    <div className="job-requirements-container">
+        <div className="job-requirements-icon-container"><GoTools size={30} /></div>
+        <div className="job-requirements-data-container">
+            <h3>Requirements</h3>
+            <ul>
+                <li>Safety Vest</li>
+                <li>Hard Hat</li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
