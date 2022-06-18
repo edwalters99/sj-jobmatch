17e1bf51c8a875560bca2260ff74dc4498342ce4
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-06-18 13:38:01 +1000

    JobReportTo, Mediaquery added to jobShiftDates

--- src/components/JobReportTo/JobReportTo.js
+++ src/components/JobReportTo/JobReportTo.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {BsPersonCircle} from 'react-icons/bs'
 import './JobReportTo.css'
 
 function JobReportTo() {
   return (
-    <div>JobReportTo</div>
+    <div className="job-reportto-container">
+    <div className="job-reportto-icon-container"><BsPersonCircle size={30} /></div>
+    <div className="job-reportto-data-container">
+        <h3>Report To</h3>
+        <p>Dave (123) 546 987</p>
+    </div>
+</div>
   )
 }
 
